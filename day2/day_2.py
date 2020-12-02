3e527ab0313e1204c42b104b6f2b208f824e93fc
Froodooo <yordiverkroost@hotmail.com>
2020-12-02 08:03:57 +0100

    Refactor day 2

--- day2/day_2.py
+++ day2/day_2.py
@@ -1,11 +1,12 @@
 import itertools
 
+
 class Day2:
     def run_a(self, input_file):
         input = self.__read(input_file)
         valid_password = self.__valid_passwords_policy_a(input)
         return valid_password
-    
+
     def run_b(self, input_file):
         input = self.__read(input_file)
         valid_password = self.__valid_passwords_policy_b(input)
@@ -15,21 +16,20 @@ class Day2:
         with open(file_name) as f:
             input = [self.__parse(line) for line in f]
         return input
-    
+
     def __parse(self, line):
         [policy, password] = line.rstrip().split(':')
         [times, letter] = policy.split()
         [lower, upper] = times.split('-')
         return [int(lower), int(upper), letter, password.lstrip()]
-    
+
     def __valid_passwords_policy_a(self, input):
         counter = 0
         for [lower, upper, letter, password] in input:
-            count = password.count(letter)
-            if count >= lower and count <= upper:
+            if password.count(letter) in range(lower, upper + 1):
                 counter += 1
         return counter
-    
+
     def __valid_passwords_policy_b(self, input):
         counter = 0
         for [position_one, position_two, letter, password] in input:
@@ -39,6 +39,7 @@ class Day2:
             if (position_one_contains_letter ^ position_two_contains_letter):
                 counter += 1
         return counter
+
 
 if __name__ == '__main__':
     day = Day2()
