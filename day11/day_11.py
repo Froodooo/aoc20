415f4f2c0f06c367790b874a4a3d36fa0e73f2a4
Yordi Verkroost <yordi.verkroost@bettyblocks.com>
2020-12-11 15:35:09 +0100

    Refactor day 11

--- day11/day_11.py
+++ day11/day_11.py
@@ -1,7 +1,12 @@
 from copy import deepcopy
+from time import time
 
 ADJACENTS = [(-1, -1), (-1, 0), (-1, 1), (0, -1),
              (0, 1), (1, -1), (1, 0), (1, 1)]
+
+EMPTY_CHAIR = 'L'
+OCCUPIED_CHAIR = '#'
+FLOOR = '.'
 
 
 def run_a(input_file):
@@ -16,95 +21,87 @@ def run_b(input_file):
 
 def _run(input_file, options):
     seats = _read(input_file)
-    dimensions = _dimensions(seats)
+    changed = True
 
-    while True:
-        seats, changed = _round(seats, dimensions, options)
-        if not changed:
-            break
+    while changed:
+        seats, changed = _round(seats, options)
 
     return _occupied(seats)
 
 
-def _round(seats, dimensions, options):
-    empty_seats = deepcopy(seats)
+def _round(seats, options):
+    rows, cols = len(seats), len(seats[0])
+    seats_copy = deepcopy(seats)
     changed = False
 
-    for row_index, row in enumerate(seats):
-        for column_index, seat in enumerate(row):
-            new_seat = seat
-            adjacent_seats = _adjacent_seats(
-                row_index, column_index, seats, dimensions, options)
-            if seat == 'L' and '#' not in adjacent_seats:
-                new_seat = '#'
+    for row in range(rows):
+        for col in range(cols):
+            adjacent_seats = _adjacent_seats(row, col, seats, options)
+            new_seat = seats[row][col]
+            if new_seat == EMPTY_CHAIR and OCCUPIED_CHAIR not in adjacent_seats:
+                new_seat = OCCUPIED_CHAIR
                 changed = True
-            elif seat == '#' and adjacent_seats.count('#') >= options['occupied_count']:
-                new_seat = 'L'
+            elif new_seat == OCCUPIED_CHAIR and adjacent_seats.count(OCCUPIED_CHAIR) >= options['occupied_count']:
+                new_seat = EMPTY_CHAIR
                 changed = True
 
-            empty_seats[row_index][column_index] = new_seat
+            seats_copy[row][col] = new_seat
 
-    return empty_seats, changed
+    return seats_copy, changed
 
 
-def _adjacent_seats(row_index, column_index, seats, dimensions, options):
+def _adjacent_seats(row, col, seats, options):
     if options['part'] == 'a':
         adjacent_seats = _direct_ajacent_seats(
-            row_index, column_index, seats, dimensions)
+            row, col, seats)
     elif options['part'] == 'b':
         adjacent_seats = _indirect_adjacent_seats(
-            row_index, column_index, seats, dimensions)
+            row, col, seats)
     return adjacent_seats
 
 
-def _direct_ajacent_seats(row_index, column_index, seats, dimensions):
+def _direct_ajacent_seats(row, col, seats):
     adjacent_seats = []
-    rows, columns = dimensions
+    rows, cols = len(seats), len(seats[0])
 
-    for y, x in ADJACENTS:
-        if 0 <= row_index - y < rows and 0 <= column_index - x < columns:
-            adjacent_seats.append(seats[row_index - y][column_index - x])
-
-    return adjacent_seats
-
-
-def _indirect_adjacent_seats(row_index, column_index, seats, dimensions):
-    adjacent_seats = []
-    rows, columns = dimensions
-
-    for y, x in ADJACENTS:
-        row_index_delta = row_index + x
-        column_index_delta = column_index + y
-
-        while _seat_is_floor(row_index_delta, column_index_delta, seats, dimensions):
-            row_index_delta += x
-            column_index_delta += y
-
-        if 0 <= row_index_delta < rows and 0 <= column_index_delta < columns:
+    for adjacent_row, adjacent_col in ADJACENTS:
+        if 0 <= row - adjacent_row < rows and 0 <= col - adjacent_col < cols:
             adjacent_seats.append(
-                seats[row_index_delta][column_index_delta])
+                seats[row - adjacent_row][col - adjacent_col])
 
     return adjacent_seats
 
 
-def _seat_is_floor(row, column, seats, dimensions):
-    rows, columns = dimensions
+def _indirect_adjacent_seats(row, col, seats):
+    adjacent_seats = []
+    rows, cols = len(seats), len(seats[0])
 
-    return 0 <= row < rows and 0 <= column < columns and seats[row][column] == '.'
+    for adjacent_row, adjacent_col in ADJACENTS:
+        row_delta = row + adjacent_col
+        col_delta = col + adjacent_row
+
+        while _seat_is_floor(row_delta, col_delta, seats):
+            row_delta += adjacent_col
+            col_delta += adjacent_row
+
+        if 0 <= row_delta < rows and 0 <= col_delta < cols:
+            adjacent_seats.append(
+                seats[row_delta][col_delta])
+
+    return adjacent_seats
 
 
-def _dimensions(seats):
-    rows = len(seats)
-    columns = len(seats[0])
+def _seat_is_floor(row, col, seats):
+    rows, cols = len(seats), len(seats[0])
 
-    return (rows, columns)
+    return 0 <= row < rows and 0 <= col < cols and seats[row][col] == FLOOR
 
 
 def _occupied(seats):
     occupied = 0
     for row in seats:
         for seat in row:
-            if seat == '#':
+            if seat == OCCUPIED_CHAIR:
                 occupied += 1
 
     return occupied
