dba54c9a661001d4e15d06b57d16416d209b8c44
Yordi Verkroost <yordi.verkroost@bettyblocks.com>
2020-12-19 17:23:41 +0100

    Scaffold day 20

--- test/test_day_17.py
+++ test/test_day_17.py
@@ -4,9 +4,10 @@ import day17.day_17
 
 
 class TestDay17(unittest.TestCase):
-    # def test_run_a_example_1(self):
-    #     output = day17.day_17.run_a('test/17_example_1.txt')
-    #     self.assertEqual(output, 848)
+    def test_run_a_example_1(self):
+        # output = day17.day_17.run_a('test/17_example_1.txt')
+        # self.assertEqual(output, 848)
+        assert True
 
 
 if __name__ == '__main__':
---
+++ test/test_day_20.py
@@ -0,0 +1,13 @@
+import unittest
+
+import day20.day_20
+
+
+class TestDay20(unittest.TestCase):
+    def test_run_a_example_1(self):
+        output = day20.day_20.run_a('test/20_example_1.txt')
+        self.assertEqual(output, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
