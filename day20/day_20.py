bdbd8897bd70520c3b9a95f1dad140ceb8fc9aae
Yordi Verkroost <yordi.verkroost@bettyblocks.com>
2020-12-20 10:23:54 +0100

    Add solution for day 20

--- day20/day_20.py
+++ day20/day_20.py
@@ -1,13 +1,102 @@
+from copy import deepcopy
+from functools import reduce
+from operator import mul
+
+
 def run_a(input_file):
-    _ = _read(input_file)
-    return 0
+    tiles = _read(input_file)
+    tile_edges = _find_edge_tiles(tiles)
+    corner_tiles = [tile_id for tile_id,
+                    tile_matches in tile_edges.items() if len(tile_matches) == 2]
+    return reduce(mul, corner_tiles)
+
+
+def _find_edge_tiles(tiles):
+    tile_edges = {}
+    for tile_id, tile in tiles.items():
+        borders = _get_tile_borders(tile)
+        edges = []
+        for other_tile_id, other_tile in tiles.items():
+            if tile_id == other_tile_id:
+                continue
+            other_borders = _get_tile_borders(other_tile)
+            touching_edges = 0
+            for rotation in range(0, 4):
+                rotated_other_borders = _rotate_borders(
+                    other_borders, rotation)
+                for flip in ['horizontal', 'vertical', 'none']:
+                    flipped_other_borders = _flip_borders(
+                        rotated_other_borders, flip)
+                    intersection = [
+                        (other_tile_id, border) for border in borders if border in flipped_other_borders]
+                    touching_edges += len(intersection)
+            if touching_edges > 0:
+                edges.append((other_tile_id, flipped_other_borders))
+        tile_edges[tile_id] = {i: borders for (i, borders) in edges}
+
+    return tile_edges
+
+
+def _rotate_borders(borders, times):
+    if times == 0:
+        return borders
+    return borders[-times:] + borders[:-times]
+
+
+def _rotate_tile(tile, times):
+    for _ in range(times):
+        tile = list(zip(*tile[::-1]))
+
+    return tile
+
+
+def _flip_borders(borders, orientation):
+    [top, right, bottom, left] = borders
+    if orientation == 'horizontal':
+        return [top[::-1], left, bottom[::-1], right]
+    elif orientation == 'vertical':
+        return [bottom, right[::-1], top, left[::-1]]
+    else:
+        return borders
+
+
+def _flip_tile(tile, orientation):
+    flipped_tile = deepcopy(tile)
+    if orientation == 'horizontal':
+        for i in range(len(tile) - 1):
+            flipped_tile[i].reverse()
+    elif orientation == 'vertical':
+        for i in range(len(tile) - 1):
+            flipped_tile[len(tile) - 1 - i] = tile[i]
+
+    return flipped_tile
+
+
+def _get_tile_borders(tile):
+    top = ''.join([pixel for pixel in tile[0]])
+    bottom = ''.join([pixel for pixel in tile[len(tile) - 1]])
+    left = ''.join([tile[row][0] for row in range(len(tile))])
+    right = ''.join([tile[row][len(tile) - 1] for row in range(len(tile))])
+
+    return [top, right, bottom, left]
 
 
 def _read(file_name):
     with open(file_name) as f:
-        input = [line for line in f]
+        input = f.read().split('\n\n')
+        tiles = {}
+        for tile in input:
+            tiles = _read_tile(tile, tiles)
 
-    return input
+    return tiles
+
+
+def _read_tile(tile, tiles):
+    tile_lines = tile.rstrip().split('\n')
+    tiles[int(tile_lines[0].split()[1][0:-1])] = \
+        [[col for col in row] for row in tile_lines[1:]]
+
+    return tiles
 
 
 def solve():
--- test/test_day_20.py
+++ test/test_day_20.py
@@ -6,7 +6,7 @@ import day20.day_20
 class TestDay20(unittest.TestCase):
     def test_run_a_example_1(self):
         output = day20.day_20.run_a('test/20_example_1.txt')
-        self.assertEqual(output, 0)
+        self.assertEqual(output, 20899048083289)
 
 
 if __name__ == '__main__':
