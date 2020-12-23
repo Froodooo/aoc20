456fb45acaeecfcefe4591720626317a83c772e7
Yordi Verkroost <yordi.verkroost@bettyblocks.com>
2020-12-23 09:39:33 +0100

    Add test for day 23 part 1

--- test/test_day_23.py
+++ test/test_day_23.py
@@ -11,6 +11,10 @@ class TestDay23(unittest.TestCase):
     def test_run_a_example_1b(self):
         output = day23.day_23.run_a('test/23_example_1.txt', 100)
         self.assertEqual(output, '67384529')
+
+    def test_run_a(self):
+        output = day23.day_23.run_a('day23/23.txt', 100)
+        self.assertEqual(output, '24987653')
     
     # def test_run_b_example_1(self):
     #     output = day23.day_23.run_b('test/23_example_1.txt', 10)
