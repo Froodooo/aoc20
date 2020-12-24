3e0bbdb66a3115251c0ad2364a65f3b6fe534f8f
Yordi Verkroost <yordi.verkroost@bettyblocks.com>
2020-12-24 08:53:40 +0100

    Scaffold day 25

---
+++ day25/day_25.py
@@ -0,0 +1,24 @@
+def run_a(input_file):
+    input = _read(input_file)
+    return 0
+
+
+def _read(file_name):
+    with open(file_name) as f:
+        input = [line for line in f]
+
+    return input
+
+
+def solve():
+    input_file = 'day25/25.txt'
+
+    result_a = run_a(input_file)
+    print(result_a)
+
+    # result_b = run_b(input_file)
+    # print(result_b)
+
+
+if __name__ == '__main__':
+    solve()
---
+++ test/test_day_25.py
@@ -0,0 +1,13 @@
+import unittest
+
+import day25.day_25
+
+
+class TestDay24(unittest.TestCase):
+    def test_run_a_example_1(self):
+        output = day25.day_25.run_a('test/25_example_1.txt')
+        self.assertEqual(output, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
