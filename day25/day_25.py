e1bdb44533b7e0a433605fa7376bf38b6cd897fc
Yordi Verkroost <yordi.verkroost@bettyblocks.com>
2020-12-25 07:20:27 +0100

    Add solution for day 25

--- day25/day_25.py
+++ day25/day_25.py
@@ -1,11 +1,41 @@
 def run_a(input_file):
-    input = _read(input_file)
-    return 0
+    [card_public_key, door_public_key] = _read(input_file)
+    subject_number = 7
+
+    card_loop_size = _find_loop_size(subject_number, card_public_key)
+    # door_loop_size = _find_loop_size(subject_number, door_public_key)
+
+    card_encryption_key = _find_encryption_key(door_public_key, card_loop_size)
+    # door_encryption_key = _find_encryption_key(card_public_key, door_loop_size)
+
+    return card_encryption_key
+
+
+def _find_loop_size(subject_number, public_key):
+    loop_size = 0
+    value = 1
+    while value != public_key:
+        value = _get_next_value(value, subject_number)
+        loop_size += 1
+
+    return loop_size
+
+
+def _find_encryption_key(subject_number, loop_size):
+    value = 1
+    for _ in range(loop_size):
+        value = _get_next_value(value, subject_number)
+    
+    return value
+
+
+def _get_next_value(value, subject_number):
+    return (value * subject_number) % 20201227
 
 
 def _read(file_name):
     with open(file_name) as f:
-        input = [line for line in f]
+        input = [int(line) for line in f]
 
     return input
 
--- test/test_day_25.py
+++ test/test_day_25.py
@@ -6,7 +6,7 @@ import day25.day_25
 class TestDay24(unittest.TestCase):
     def test_run_a_example_1(self):
         output = day25.day_25.run_a('test/25_example_1.txt')
-        self.assertEqual(output, 0)
+        self.assertEqual(output, 14897079)
 
 
 if __name__ == '__main__':
